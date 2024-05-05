710a4144d06cb3c46e632a862a04602583fb8748
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-05-05 07:56:31 +0500

    refactor: Update User model and migration
    - Rename 'name' field to 'fullname' in User model and migration
    - Add 'role' field with default value of 'student' in User migration
    - Update DatabaseSeeder to create test users with fullname, email, role, and password

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
+            $table->string('role')->default('student');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'fullname' => 'John Doe',
+            'email' => 'johndoe@hey.com',
+            'role' => 'student',
+            'password' => bcrypt('password')
         ]);
+        User::create([
+            'fullname' => 'Jane Doe',
+            'email' => 'janedoe@hey.com',
+            'role' => 'teacher',
+            'password' => bcrypt('password')
+        ]);
+
+        
+
     }
 }
